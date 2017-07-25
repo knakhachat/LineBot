9b4586c95c3cc242fbb1ce38558b3360fcf9a8ea
Aunza <30429151+knakhachat@users.noreply.github.com>
2017-07-25 17:12:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
+	function quote_generate()
+	{
+		$quote = array("ไปนอนไป๊",
+		"นอนดึกเหรอ","ไปทำอะไรมา","ติดซีรี่ย์อ่ะดิ","เหมือนกันเลย");
 
-$choice1="บ้าที่สุด";
-$choice2="กรี๊ดดดด";
-$choice3="ดีใจจังคันหลังก็ลาว";
-$choice4="สนุกมั้ย";
-$arr=array ( $choice1,$choice2,$choice3,$choiec4); 
-shuffle($arr);
-foreach ($num as $arr) {
-    echo $num;
-}
-echo "Hello world";
+		return($quote[rand(0, count($quote) - 1)]);
+	}
+
+	echo quote_generate();
+?>
