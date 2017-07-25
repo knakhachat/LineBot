857386c6a36b06d5faa5b877a643944ca2c1f7bd
Aunza <30429151+knakhachat@users.noreply.github.com>
2017-07-25 16:51:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
+
+$choice1="บ้าที่สุด";
+$choice2="กรี๊ดดดด";
+$choice3="ดีใจจังคันหลังก็ลาว";
+$choice4="สนุกมั้ย";
+$arr=array ( $choice1,$choice2,$choice3,$choiec4); 
+shuffle($arr);
+foreach ($textreply as $arr) {
+    echo "$textreply ";
+}
 echo "Hello world";
