dee420264de253f3546a2123d53fef47c9bf4104
Aunza <30429151+knakhachat@users.noreply.github.com>
2017-07-31 10:52:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-	function quote_generate()
-	{
-		$quote = array("ไปนอนไป๊",
-		"นอนดึกเหรอ","ไปทำอะไรมา","ติดซีรี่ย์อ่ะดิ","เหมือนกันเลย");
-
-		return($quote[rand(0, count($quote) - 1)]);
-	}
-
-	echo quote_generate();
-?>
+  $string = 'Hello World!';
+  if(stristr($string, 'Hello') === TRUE) {
+    echo '"เจอเว้ย"';
+  }
+// outputs: "earth" not found in string
+?> 
