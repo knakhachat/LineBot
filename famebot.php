02e39fa93f8c7b1ba365989dc0dd3c97aaa8a654
Aunza <30429151+knakhachat@users.noreply.github.com>
2018-03-14 17:38:10 +0700

    Update famebot.php

--- famebot.php
+++ famebot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('line-bot-api/php/line-bot.php');
+include ('./line-bot-api/php/line-bot.php');
 
 $channelSecret = '4b05c24b604f3ad4d9336963f3b11351';
 $access_token  = 'SRVmlBWe3AGaCl6IEx9LuU2h5lK+SYnym9C6TT/qC0QjlZDBntG3MZYNKeCToylKaf/xqUF8JKYdMnSSxRSUrh5YLSdtSc7pZDUHH3G+2vcNIimvwWMWMpQq1oyIz2Jum5KNqeFGRkdxss0aGyWj2QdB04t89/1O/w1cDnyilFU=';
@@ -23,3 +23,5 @@ if (!empty($bot->isEvents)) {
 	exit();
 
 }
+
+?>
